23e739308177cf1408d9c3922d5d3bd806f1e5a3
Charles Butterworth <charlessbutterworth@gmail.com>
2020-10-07 14:10:26 -0500

    Added in the scripts and js files

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,43 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Ricky",
+        food: "Fish, Algea",
+        length: 12.3-15.2,
+        location: "Amazon River Basin",
+        species: "Symphysodon aequifasciatus",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Blue_Discus.jpg/440px-Blue_Discus.jpg"
+    },
+    {
+        name: "Ricky's Twin",
+        food: "Carrots, Apples",
+        length: 12.3-15.2,
+        location: "Greece",
+        species: "Symphysodon aequifasciatus",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Blue_Discus.jpg/440px-Blue_Discus.jpg"
+    },
+    {
+        name: "Ricky's Half Brother",
+        food: "cereal, worms, insects",
+        length: 12.3-15.2,
+        location: "Japan",
+        species: "Symphysodon aequifasciatus",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Blue_Discus.jpg/440px-Blue_Discus.jpg"
+    },
+    {
+        name: "Ricky's cousin",
+        food: "Pizza, cheese",
+        length: 12.3-15.2,
+        location: "USA",
+        species: "Symphysodon aequifasciatus",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Blue_Discus.jpg/440px-Blue_Discus.jpg"
+    }
+]
+export const useFish = () => {
+    return fishCollection.slice 
+}
